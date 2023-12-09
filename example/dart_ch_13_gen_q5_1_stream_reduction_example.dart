692a46cbbc7c2409288bc4ee419321a132136857
M I MITHU <mithu7ete@gmail.com>
2023-12-09 15:05:51 +0600

    Done

---
+++ example/dart_ch_13_gen_q5_1_stream_reduction_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_ch_13_gen_q5_1_stream_reduction/dart_ch_13_gen_q5_1_stream_reduction.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
